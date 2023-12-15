7d0bdee1c4580598cd64c2a8d951238868c76e57
Dor Peled <knat.dev.93@gmail.com>
2023-12-15 15:42:00 +0200

    premake updated

--- premake5.lua
+++ premake5.lua
@@ -25,6 +25,10 @@ project "ImGui"
 		systemversion "latest"
 		cppdialect "C++17"
 
+        defines {
+            "IMGUI_API=__declspec(dllexport)"
+        }
+
 	filter "system:linux"
 		pic "On"
 		systemversion "latest"
